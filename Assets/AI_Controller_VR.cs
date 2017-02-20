685c8819a3f754879f4b03463c9d76a172a5c4ea
OscarDorado1138 <OscarDorado1138@Berkeley.edu>
2017-02-19 18:11:57 -0800

    AI Variables

--- Assets/AI_Controller_VR.cs
+++ Assets/AI_Controller_VR.cs
@@ -18,12 +18,32 @@ public class AI_Controller_VR : MonoBehaviour {
 
     int frame_interval = 5;
 
+
+
+
+    GameObject turret_object;
+    GameObject cannon_base;
+    GameObject turret_base;
+
+    // Network Values
+    float pos_x;
+    float pos_y;
+    float pos_z;
+    float rot_x;
+    float rot_y;
+    float rot_z;
+    float turret_base_rotation_y;
+    float cannon_base_rotation_x;
+
     // Use this for initialization
     void Start()
     {
+        turret_object = transform.FindChild("Turret_Objects").gameObject;
+        cannon_base = transform.FindChild("Cannon_Base").gameObject;
+        turret_base = GetComponentInChildren<Turret_Horizontal_CS>().gameObject;
     }
 
-  
+
 
     public void Prep()
     {
@@ -51,11 +71,6 @@ public class AI_Controller_VR : MonoBehaviour {
 
             reliable_message = n_manager_script.reliable_message;
 
-            if (current_player == 2)
-            {
-                
-            }
-            else {
                 if (reliable_message)
                 {
                     if (n_manager_script.server_read_client_reliable_buffer(2) == 1 && ai_id == 1)
@@ -75,6 +90,20 @@ public class AI_Controller_VR : MonoBehaviour {
                         transform.FindChild("Turret").GetComponent<Damage_Control_CS>().Penetration();
                     }
                 }
+
+
+
+            update_world_state();
+
+
+            if (current_player == 1)
+            {
+                server_get_values_to_send();
+            }
+
+            else
+            {
+                client_update_values();
             }
 
 
@@ -88,6 +117,90 @@ public class AI_Controller_VR : MonoBehaviour {
         n_manager_script.send_reliable_from_client(ai_id + 1, 1);
 
     }
+
+
+
+
+    void update_world_state()
+    {
+        // Client
+        if (current_player == 1)
+        {
+
+        }
+        // Server
+        else
+        {
+            this.transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(pos_x, pos_y, pos_z), 0.1f);
+            this.transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(rot_x, rot_y, rot_z), 0.1f);
+
+            if (Quaternion.Angle(turret_object.transform.localRotation, Quaternion.Euler(0, turret_base_rotation_y, 0)) > 0.1f)
+            {
+                turret_base.transform.localRotation = Quaternion.Lerp(turret_base.transform.localRotation, Quaternion.Euler(0, turret_base_rotation_y, 0), 0.1f);
+                //turret_base.transform.localRotation = Quaternion.Euler(0, turret_base_rotation_y, 0);
+
+            }
+            cannon_base.transform.localRotation = Quaternion.Lerp(cannon_base.transform.localRotation, Quaternion.Euler(cannon_base_rotation_x, 0, 0), 0.1f);
+            //cannon_base.transform.localRotation = Quaternion.Euler(cannon_base_rotation_x, 0, 0);
+
+        
+
+        }
+
+
+
+    }
+
+
+
+
+    void server_get_values_to_send()
+    {
+
+
+
+        float[] all_values = { transform.localPosition.x,
+                               transform.localPosition.y,
+                               transform.localPosition.z,
+                               transform.localRotation.eulerAngles.x,
+                               transform.localRotation.eulerAngles.y,
+                               transform.localRotation.eulerAngles.z,
+                               turret_base.transform.localEulerAngles.y,
+                               cannon_base.transform.localEulerAngles.x
+
+                             };
+
+       
+
+        n_manager_script.send_from_server(11 + ai_id, all_values);
+
+
+    }
+
+
+
+    void client_update_values()
+    {
+        float[] all_values = n_manager_script.client_read_server_buffer(11 + ai_id);
+        pos_x = all_values[0];
+        pos_y = all_values[1];
+        pos_z = all_values[2];
+        rot_x = all_values[3];
+        rot_y = all_values[4];
+        rot_z = all_values[5];
+        turret_base_rotation_y = all_values[6];
+        cannon_base_rotation_x = all_values[7];
+
+    }
+
+
+
+
+
+
+
+
+
 
 
 
