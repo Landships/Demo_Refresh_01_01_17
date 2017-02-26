dab1c3862f8936da5e07566e7888d1bafea21fc2
Ian McAulay <imcaulay@berkeley.edu>
2017-02-26 15:22:09 -0800

    hotkey should be working for gunner as well now

--- Assets/Set_Camera_Position_on_Hotkey.cs
+++ Assets/Set_Camera_Position_on_Hotkey.cs
@@ -5,30 +5,45 @@ using UnityEngine;
 public class Set_Camera_Position_on_Hotkey : MonoBehaviour {
 
     public GameObject cameraRig;
-    public GameObject correctPos;
+    public GameObject correctDriverPos;
+    public GameObject correctGunnerPos;
     public GameObject cameraHead;
+    byte current_player = 1;
+    GameObject n_manager;
+    network_manager n_manager_script;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
-	// Update is called once per frame
 	void Update () {
 
         if (Input.GetKeyDown("c") == true) {
 
-            
+            Vector3 distanceToMove = new Vector3(0, 0, 0);
 
-            Vector3 distanceToMove =  correctPos.transform.position - cameraHead.transform.position;
+            if (current_player == 1) {
 
-            Debug.Log(distanceToMove);
+                distanceToMove = correctDriverPos.transform.position - cameraHead.transform.position;
+
+            } else if (current_player == 2) {
+
+                distanceToMove = correctGunnerPos.transform.position - cameraHead.transform.position;
+
+            } else {
+                Debug.Log("If you got here it means we added more players which is pretty cool :)");
+                // If there are more players, just add an else if condition for each where the correct
+                // position is used to calculate distanceToMove
+            }
 
             cameraRig.transform.Translate(distanceToMove, Space.World);
 
         }
 
-
-
     }
+
+    public void Prep() {
+        n_manager = GameObject.Find("Custom Network Manager(Clone)");
+        n_manager_script = n_manager.GetComponent<network_manager>();
+        current_player = (byte)(n_manager_script.client_players_amount);
+    }
+
+
 }
