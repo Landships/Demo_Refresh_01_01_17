9145025cfc0ff5f083c7b02f7a174774b7173545
Neverblade <james97lin@gmail.com>
2017-02-26 15:02:26 -0800

    Added script to reset camera position, not yet working for gunner though

---
+++ Assets/Set_Camera_Position_on_Hotkey.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Set_Camera_Position_on_Hotkey : MonoBehaviour {
+
+    public GameObject cameraRig;
+    public GameObject correctPos;
+    public GameObject cameraHead;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("c") == true) {
+
+            
+
+            Vector3 distanceToMove =  correctPos.transform.position - cameraHead.transform.position;
+
+            Debug.Log(distanceToMove);
+
+            cameraRig.transform.Translate(distanceToMove, Space.World);
+
+        }
+
+
+
+    }
+}
