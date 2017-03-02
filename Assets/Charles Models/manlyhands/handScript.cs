4ac81d71b9a79a76c2c8c1daa8705186072a3a37
rsl18 <rsluo@zonemail.clpccd.edu>
2017-03-01 22:00:20 -0800

    maybe?

--- Assets/Charles Models/manlyhands/handScript.cs
+++ Assets/Charles Models/manlyhands/handScript.cs
@@ -4,53 +4,24 @@ using System.Collections;
 public class handScript : MonoBehaviour {
     private Animator handAnimator;
     private float currentBlend = 0.0f;
-    //public SteamVR_Controller.Device VR_Controller_Script;
-    private bool canGrab = false;
-    private int deviceIndex = -1;
+    private VRTK.VRTK_ControllerEvents controller_events;
+    private VRTK.VRTK_InteractTouch touch_events;
 
     // Use this for initialization
     void Start () {
-        //Debug.Log("handscript starting");
         handAnimator = GetComponent<Animator>();
-        /*
-        if (gameObject.transform.name == "left hand")
-        {
-            Debug.Log("is left hand");
-            deviceIndex = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.FarthestRight);
-        }
-        else if (gameObject.transform.name == "right hand")
-        {
-            Debug.Log("is right hand");
+        controller_events = transform.parent.GetComponent<VRTK.VRTK_ControllerEvents>();
 
-            deviceIndex = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.FarthestLeft);
-        }
-        */
 
     }
 
     // Update is called once per frame
     void Update () {
-        //Debug.Log("device index: " + deviceIndex);
-        /*
-        if (deviceIndex != -1 && SteamVR_Controller.Input(deviceIndex).GetPress(SteamVR_Controller.ButtonMask.Trigger))
-        {
-            //Debug.Log("is pulling trigger on hand");
-            handAnimator.SetFloat("handBlend", 1.0f, 0.1f, Time.deltaTime);
-        }
-        else if (transform.parent.GetComponent<InteractPromptScript>().canGrab)
-        {
-            handAnimator.SetFloat("handBlend", 0.5f, 0.1f, Time.deltaTime);
-        }
-        else
-        {
-            handAnimator.SetFloat("handBlend", 0.0f, 0.1f, Time.deltaTime);
-        }*/
-
-        if (gameObject.transform.parent.GetComponent<PseudoHand>().trigger_on)
+        if (controller_events.triggerClicked)
         {
             handAnimator.SetFloat("handBlend", 1.0f, 0.1f, Time.deltaTime);
         }
-        else if (transform.parent.GetComponent<InteractPromptScript>().canGrab)
+        else if (touch_events.GetTouchedObject() != null)
         {
             handAnimator.SetFloat("handBlend", 0.5f, 0.1f, Time.deltaTime);
         }
@@ -66,6 +37,6 @@ public class handScript : MonoBehaviour {
 
     public void hapticFeedBack()
     {
-        SteamVR_Controller.Input(deviceIndex).TriggerHapticPulse(250);
+        //SteamVR_Controller.Input(deviceIndex).TriggerHapticPulse(250);
     }
 }
